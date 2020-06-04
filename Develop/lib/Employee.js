07bf3e9977d25189d3fa7fefb24653639da3a287
Tyler Roth <tylerroth1535@gmail.com>
2020-06-03 22:58:47 -0400

    passed all Employee tests

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,8 +1,30 @@
 // TODO: Write code to define and export the Employee class
 class Employee{
-    constructor(){
-        
+    constructor(name, id, email, role){
+        this.name = name;
+        this.id = id;
+        this.email = email; 
+        this.role= role;
     }
-;}
+    getName(){
+        return this.name;
+    };
+
+    getId(){
+        return this.id;
+    };
+
+    getEmail(){
+        return this.email;
+    };
+
+    getRole(){
+        return "Employee";
+    };
+};
+
+const e = new Employee ();
+
+console.log(e);
 
 module.exports = Employee;
