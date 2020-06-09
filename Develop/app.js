b4cbbb13659f62eed979fc17f88463b5433aad70
Tyler Roth <tylerroth1535@gmail.com>
2020-06-08 21:40:02 -0400

    updated readme

--- Develop/app.js
+++ Develop/app.js
@@ -126,7 +126,19 @@ const teamInfo = () =>{
         },{
             type: "input",
             message: "Please enter the employee's email address.",
-            name: "email"
+            name: "email",
+            validate: function (email) {
+  
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+    
+                if (valid) {
+                  console.log("Great job");
+                    return true;
+                } else {
+                    console.log(".  Please enter a valid email")
+                    return false;
+                }
+            }
         },{
             type: "list",
             choices: ["Intern", "Engineer", "Manager"],
